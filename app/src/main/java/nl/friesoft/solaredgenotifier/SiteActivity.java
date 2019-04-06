a6c29e455ebc233ab9afe0e68140668265ff7738
bart <bart@piranhastuff.nl>
2019-04-06 14:13:09 +0200

    remove placeholders in Site Activity

--- app/src/main/java/nl/friesoft/solaredgenotifier/SiteActivity.java
+++ app/src/main/java/nl/friesoft/solaredgenotifier/SiteActivity.java
@@ -25,7 +25,7 @@ public class SiteActivity extends AppCompatActivity implements ISolarEdgeListene
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_installation);
+        setContentView(R.layout.activity_site);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
