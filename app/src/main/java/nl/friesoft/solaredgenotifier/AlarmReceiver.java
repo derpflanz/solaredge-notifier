7d695a981ac536df7268c9b3d2802ba31fdb44d2
bart <bart@piranhastuff.nl>
2019-10-26 16:39:17 +0200

    removed test code

--- app/src/main/java/nl/friesoft/solaredgenotifier/AlarmReceiver.java
+++ app/src/main/java/nl/friesoft/solaredgenotifier/AlarmReceiver.java
@@ -29,11 +29,8 @@ public class AlarmReceiver extends BroadcastReceiver implements ISolarEdgeListen
     Persistent persistent;
     Context context;
 
-//    private static final long INTERVAL_ERROR = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
-//    private static final long INTERVAL_SUCCESS = AlarmManager.INTERVAL_DAY;
-
-    private static final long INTERVAL_ERROR = 10000;
-    private static final long INTERVAL_SUCCESS = 60000;
+    private static final long INTERVAL_ERROR = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
+    private static final long INTERVAL_SUCCESS = AlarmManager.INTERVAL_DAY;
 
     // set interval to null to cancel the alarm
     public static void setAlarm(Context ctx, Long interval) {
